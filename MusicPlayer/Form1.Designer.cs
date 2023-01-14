29a29f13a291899612d27bd755c7c84eebc01d6a
Yura <Yura@DELL-Y>
2023-01-14 22:28:47 +0200

    Add project files.

---
+++ MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace MusicPlayer
+{
+	partial class MusicPlayerApp
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.PlayPauseButton = new System.Windows.Forms.PictureBox();
+			this.StopButton = new System.Windows.Forms.PictureBox();
+			this.VolumeBar = new System.Windows.Forms.TrackBar();
+			this.VolumePercentage = new System.Windows.Forms.Label();
+			this.VolumeOnOffButton = new System.Windows.Forms.PictureBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.TimeElapsed = new System.Windows.Forms.Label();
+			this.MediaDuration = new System.Windows.Forms.Label();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.playlistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.videoView1 = new LibVLCSharp.WinForms.VideoView();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.addSubtitlesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			((System.ComponentModel.ISupportInitialize)(this.PlayPauseButton)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.StopButton)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.VolumeOnOffButton)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// PlayPauseButton
+			// 
+			this.PlayPauseButton.BackColor = System.Drawing.SystemColors.Control;
+			this.PlayPauseButton.Image = global::MusicPlayer.Properties.Resources.play;
+			this.PlayPauseButton.InitialImage = global::MusicPlayer.Properties.Resources.play;
+			this.PlayPauseButton.Location = new System.Drawing.Point(289, 351);
+			this.PlayPauseButton.Name = "PlayPauseButton";
+			this.PlayPauseButton.Size = new System.Drawing.Size(40, 40);
+			this.PlayPauseButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.PlayPauseButton.TabIndex = 3;
+			this.PlayPauseButton.TabStop = false;
+			this.PlayPauseButton.Click += new System.EventHandler(this.PlayPauseButton_Click);
+			// 
+			// StopButton
+			// 
+			this.StopButton.Image = global::MusicPlayer.Properties.Resources.stop_74;
+			this.StopButton.Location = new System.Drawing.Point(335, 351);
+			this.StopButton.Name = "StopButton";
+			this.StopButton.Size = new System.Drawing.Size(40, 40);
+			this.StopButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.StopButton.TabIndex = 4;
+			this.StopButton.TabStop = false;
+			this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+			// 
+			// VolumeBar
+			// 
+			this.VolumeBar.Location = new System.Drawing.Point(525, 355);
+			this.VolumeBar.Maximum = 100;
+			this.VolumeBar.Name = "VolumeBar";
+			this.VolumeBar.Size = new System.Drawing.Size(161, 56);
+			this.VolumeBar.TabIndex = 0;
+			this.VolumeBar.TabStop = false;
+			this.VolumeBar.Value = 50;
+			this.VolumeBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+			this.VolumeBar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.VolumeBar_KeyDown);
+			// 
+			// VolumePercentage
+			// 
+			this.VolumePercentage.AutoSize = true;
+			this.VolumePercentage.Location = new System.Drawing.Point(680, 357);
+			this.VolumePercentage.Name = "VolumePercentage";
+			this.VolumePercentage.Size = new System.Drawing.Size(37, 20);
+			this.VolumePercentage.TabIndex = 6;
+			this.VolumePercentage.Text = "50%";
+			// 
+			// VolumeOnOffButton
+			// 
+			this.VolumeOnOffButton.Image = global::MusicPlayer.Properties.Resources.soundOn;
+			this.VolumeOnOffButton.Location = new System.Drawing.Point(479, 351);
+			this.VolumeOnOffButton.Name = "VolumeOnOffButton";
+			this.VolumeOnOffButton.Size = new System.Drawing.Size(40, 40);
+			this.VolumeOnOffButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.VolumeOnOffButton.TabIndex = 7;
+			this.VolumeOnOffButton.TabStop = false;
+			this.VolumeOnOffButton.Click += new System.EventHandler(this.VolumeOnOffButton_Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(7, 339);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(779, 10);
+			this.progressBar1.TabIndex = 8;
+			this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+			// 
+			// TimeElapsed
+			// 
+			this.TimeElapsed.AutoSize = true;
+			this.TimeElapsed.Location = new System.Drawing.Point(7, 352);
+			this.TimeElapsed.Name = "TimeElapsed";
+			this.TimeElapsed.Size = new System.Drawing.Size(44, 20);
+			this.TimeElapsed.TabIndex = 9;
+			this.TimeElapsed.Text = "00:00";
+			// 
+			// MediaDuration
+			// 
+			this.MediaDuration.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.MediaDuration.AutoSize = true;
+			this.MediaDuration.Location = new System.Drawing.Point(746, 352);
+			this.MediaDuration.Name = "MediaDuration";
+			this.MediaDuration.Size = new System.Drawing.Size(44, 20);
+			this.MediaDuration.TabIndex = 10;
+			this.MediaDuration.Text = "00:00";
+			this.MediaDuration.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.playlistToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+			this.menuStrip1.TabIndex = 11;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.addSubtitlesToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+			this.openToolStripMenuItem.Text = "Open";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			// 
+			// playlistToolStripMenuItem
+			// 
+			this.playlistToolStripMenuItem.Name = "playlistToolStripMenuItem";
+			this.playlistToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+			this.playlistToolStripMenuItem.Text = "Playlist";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+			this.aboutToolStripMenuItem.Text = "About";
+			// 
+			// videoView1
+			// 
+			this.videoView1.BackColor = System.Drawing.Color.Black;
+			this.videoView1.Location = new System.Drawing.Point(12, 42);
+			this.videoView1.MediaPlayer = null;
+			this.videoView1.Name = "videoView1";
+			this.videoView1.Size = new System.Drawing.Size(774, 293);
+			this.videoView1.TabIndex = 12;
+			this.videoView1.Text = "videoView1";
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// addSubtitlesToolStripMenuItem
+			// 
+			this.addSubtitlesToolStripMenuItem.Name = "addSubtitlesToolStripMenuItem";
+			this.addSubtitlesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+			this.addSubtitlesToolStripMenuItem.Text = "Add Subtitles";
+			this.addSubtitlesToolStripMenuItem.Click += new System.EventHandler(this.addSubtitlesToolStripMenuItem_Click);
+			// 
+			// MusicPlayerApp
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.videoView1);
+			this.Controls.Add(this.MediaDuration);
+			this.Controls.Add(this.TimeElapsed);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.VolumeOnOffButton);
+			this.Controls.Add(this.VolumePercentage);
+			this.Controls.Add(this.VolumeBar);
+			this.Controls.Add(this.StopButton);
+			this.Controls.Add(this.PlayPauseButton);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MusicPlayerApp";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "MusicPlayerApp";
+			((System.ComponentModel.ISupportInitialize)(this.PlayPauseButton)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.StopButton)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.VolumeOnOffButton)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private PictureBox PlayPauseButton;
+		private PictureBox StopButton;
+		private TrackBar VolumeBar;
+		private Label VolumePercentage;
+		private PictureBox VolumeOnOffButton;
+		private ProgressBar progressBar1;
+		private Label TimeElapsed;
+		private Label MediaDuration;
+		private MenuStrip menuStrip1;
+		private ToolStripMenuItem fileToolStripMenuItem;
+		private ToolStripMenuItem playlistToolStripMenuItem;
+		private ToolStripMenuItem aboutToolStripMenuItem;
+		private ToolStripMenuItem openToolStripMenuItem;
+		private LibVLCSharp.WinForms.VideoView videoView1;
+		private System.Windows.Forms.Timer timer1;
+		private ToolStripMenuItem addSubtitlesToolStripMenuItem;
+	}
+}
---
+++ MusicPlayer/Form1.cs
@@ -0,0 +1,327 @@
+using MusicPlayer.Properties;
+using LibVLCSharp.Shared;
+using System.IO;
+using System.Threading;
+using System.Reflection;
+using System.Timers;
+using System.Globalization;
+
+namespace MusicPlayer
+{
+	public partial class MusicPlayerApp : Form
+	{
+		private LibVLC _libVLC;
+		private MediaPlayer _mp;
+		private Media _media;
+
+		private bool _isFullScreen = false;
+		private int _previousVolume;
+		private Size oldFormSize;
+		private Size oldVideoSize;
+		private Point oldVideoLocation;
+
+		public MusicPlayerApp()
+		{
+			InitializeComponent();
+			Core.Initialize();
+			oldVideoSize = videoView1.Size;
+			oldFormSize = Size;
+			oldVideoLocation = videoView1.Location;
+			//VLC 
+			_libVLC = new LibVLC();
+			_mp = new MediaPlayer(_libVLC);
+			videoView1.MediaPlayer = _mp;
+			_mp.Volume = _previousVolume = 50;
+			_mp.EndReached += _mp_EndReached;
+
+			KeyPreview = true;
+			KeyDown += new KeyEventHandler(ShortcutEvent);
+		}
+
+		public void ShortcutEvent(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape && _isFullScreen) // from fullscreen to window
+			{
+				ExitFullScreen();
+				return;
+			}
+
+			if (e.KeyCode == Keys.F || e.KeyCode == Keys.F12)
+			{
+				if (_isFullScreen)
+					ExitFullScreen();
+				else
+					GoFullScreen();
+				return;
+			}
+
+			if (e.KeyCode == Keys.M)
+				VolumeOnOffButton_Click(sender, e);
+
+			if (IsMediaSelected())
+			{
+				if (e.KeyCode == Keys.Space) // Pause and Play
+				{
+					if (_mp.State == VLCState.Playing) // if is playing
+						Pause();
+					else // it's not playing?
+						Play(); // play
+					return;
+				}
+				float TenSecInPercentage = 10000f / _mp.Length;
+
+				if (e.KeyCode == Keys.Left) // skip 10sec backwards
+				{
+					if (_mp.Position >= TenSecInPercentage)
+						_mp.Position -= TenSecInPercentage;
+					else
+						_mp.Position = 0;
+				}
+
+				if (e.KeyCode == Keys.Right) // skip 10sec forwards
+				{
+					if (_mp.Position <= 1 - TenSecInPercentage)
+						_mp.Position += TenSecInPercentage;
+					else
+						_mp.Position = 1;
+				}
+			}
+		}
+
+		private void GoFullScreen()
+		{
+			menuStrip1.Visible = false; // goodbye menu strip
+			videoView1.Location = new Point(0, 0); // remove the offset
+			FormBorderStyle = FormBorderStyle.None; // change form style
+			WindowState = FormWindowState.Maximized;
+			videoView1.Size = Size; // make video the same size as the form
+			_isFullScreen = true;
+		}
+
+		private void ExitFullScreen()
+		{
+			FormBorderStyle = FormBorderStyle.Sizable; // change form style
+			WindowState = FormWindowState.Normal; // back to normal size
+			Size = oldFormSize;
+			menuStrip1.Visible = true; // the return of the menu strip 
+			videoView1.Size = oldVideoSize; // make video the same size as the form
+			videoView1.Location = oldVideoLocation; // remove the offset
+			_isFullScreen = false;
+		}
+
+		private void _mp_EndReached(object? sender, EventArgs e)
+		{
+			ThreadPool.QueueUserWorkItem(_ => _mp.Stop());
+			PlayPauseButton.Image = Resources.play;
+			timer1.Stop();
+			progressBar1.Value = 0;
+			TimeElapsed.Text = "00:00";
+		}
+
+		private void PlayPauseButton_Click(object sender, EventArgs e)
+		{
+			if (!IsMediaSelected())
+				return;
+
+			if (!_mp.IsPlaying)
+			{
+				Play();
+				return;
+			}
+			Pause();
+		}
+
+		private void Pause()
+		{
+			_mp.Pause();
+			PlayPauseButton.Image = Resources.play;
+			timer1.Stop();
+		}
+
+		private void Play()
+		{
+			_mp.Play();
+			PlayPauseButton.Image = Resources.pause;
+			timer1.Start();
+		}
+
+		private bool IsMediaSelected()
+		{
+			if (_media == null)
+			{
+				MessageBox.Show("Please select a mediafile!", "Error", MessageBoxButtons.OK);
+				return false;
+			}
+			return true;
+		}
+
+		private void StopButton_Click(object sender, EventArgs e)
+		{
+			if (!IsMediaSelected())
+				return;
+
+			Stop();
+		}
+
+		private void Stop()
+		{
+			_mp.Stop();
+			PlayPauseButton.Image = Resources.play;
+			timer1.Stop();
+			progressBar1.Value = 0;
+			TimeElapsed.Text = "00:00";
+		}
+
+		private void trackBar1_Scroll(object sender, EventArgs e)
+		{
+			if (VolumeBar.Value == 0)
+				VolumeOnOffButton.Image = Resources.soundOff;
+			else
+				VolumeOnOffButton.Image = Resources.soundOn;
+
+			SetVolumeAsVolumeBarValue();
+		}
+
+		private void SetVolumeAsVolumeBarValue()
+		{
+			_mp.Volume = VolumeBar.Value;
+			_previousVolume = _mp.Volume;
+			VolumePercentage.Text = VolumeBar.Value.ToString() + "%";
+		}
+
+		private void VolumeOnOffButton_Click(object sender, EventArgs e)
+		{
+			if (_mp.Volume != 0)
+			{
+				_previousVolume = _mp.Volume;
+				_mp.Volume = 0;
+				VolumeBar.Value = 0;
+				VolumeOnOffButton.Image = Resources.soundOff;
+			}
+			else
+			{
+				_mp.Volume = _previousVolume;
+				VolumeBar.Value = _previousVolume;
+				VolumeOnOffButton.Image = Resources.soundOn;
+			}
+			VolumePercentage.Text = VolumeBar.Value.ToString() + "%";
+		}
+
+		private void CloseApp_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void openToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (_mp.IsPlaying)
+				Pause();
+
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Filter = "All Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV";
+
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				Stop();
+				_media = new Media(_libVLC, ofd.FileName, FromType.FromPath);
+			}
+			else
+			{
+				if (_mp.State == VLCState.Paused)
+					Play();
+				return;
+			}
+
+			_mp.Media = _media;
+			_mp.Media.AddOption(":start-paused");
+			_mp.Play();
+			Thread.Sleep(100);
+			_mp.Media.AddOption(":no-start-paused");
+
+			TimeSpan duration = new TimeSpan(_mp.Media.Duration * 10000);
+			if (duration.Hours != 0)
+				MediaDuration.Text = $"{duration.Hours:00}:{duration.Minutes:00}:{duration.Seconds:00}";
+			else
+				MediaDuration.Text = $"{duration.Minutes:00}:{duration.Seconds:00}";
+
+			progressBar1.Maximum = (int)duration.TotalSeconds;
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			TimeSpan timerPosition = new TimeSpan((long)(_mp.Position * _mp.Media.Duration * 10000));
+
+			if (timerPosition.Hours != 0)
+				TimeElapsed.Text = $"{timerPosition.Hours:00}:{timerPosition.Minutes:00}:{timerPosition.Seconds:00}";
+			else
+				TimeElapsed.Text = $"{timerPosition.Minutes:00}:{timerPosition.Seconds:00}";
+
+			if (_mp.IsPlaying)
+				progressBar1.Value = (int)timerPosition.TotalSeconds;
+		}
+
+		private void progressBar1_Click(object sender, EventArgs e)
+		{
+			if (!IsMediaSelected())
+				return;
+
+			float absoluteMouse = (PointToClient(MousePosition).X - progressBar1.Bounds.X);
+			float calcFactor = progressBar1.Width / (float)progressBar1.Maximum;
+			float relativeMouse = absoluteMouse / calcFactor;
+
+			progressBar1.Value = Convert.ToInt32(relativeMouse);
+
+			TimeSpan selectedTime = new TimeSpan((long)progressBar1.Value * 10000000);
+
+			if (selectedTime.Hours != 0)
+				TimeElapsed.Text = $"{selectedTime.Hours:00}:{selectedTime.Minutes:00}:{selectedTime.Seconds:00}";
+			else
+				TimeElapsed.Text = $"{selectedTime.Minutes:00}:{selectedTime.Seconds:00}";
+
+			_mp.Position = (float)(selectedTime.TotalSeconds / (float)(_mp.Length / 1000));
+		}
+
+		private void VolumeBar_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+			{
+				e.SuppressKeyPress = true;
+				return;
+			}
+
+			if (e.KeyCode == Keys.Up)
+			{
+				e.SuppressKeyPress = true;
+				VolumeBar.Value += 1;
+			}
+
+			if (e.KeyCode == Keys.Down)
+			{
+				e.SuppressKeyPress = true;
+				VolumeBar.Value -= 1;
+			}
+			SetVolumeAsVolumeBarValue();
+		}
+
+		private void addSubtitlesToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (!IsMediaSelected())
+				return;
+
+			if (_mp.IsPlaying)
+				Pause();
+
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Filter = "All Subtitle Files|*.vtt;*.srt";
+
+			if (ofd.ShowDialog() == DialogResult.OK)
+				_mp.AddSlave(MediaSlaveType.Subtitle, "file:///" + ofd.FileName, true);
+			else
+			{
+				if (_mp.State == VLCState.Paused)
+					Play();
+				return;
+			}
+		}
+	}
+}
